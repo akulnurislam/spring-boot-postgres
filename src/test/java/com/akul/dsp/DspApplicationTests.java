855242eb319482907eae39add72f5139576d784d
akul <nurislamimanudin@gmail.com>
2023-05-10 20:58:44 +0700

    init spring-boot

---
+++ src/test/java/com/akul/dsp/DspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akul.dsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
