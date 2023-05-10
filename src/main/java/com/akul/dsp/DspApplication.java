8096a5bf1f2bea1b1a9bf36ed46d2a29cae4b975
akul <nurislamimanudin@gmail.com>
2023-05-11 05:50:51 +0700

    format

--- src/main/java/com/akul/dsp/DspApplication.java
+++ src/main/java/com/akul/dsp/DspApplication.java
@@ -9,5 +9,4 @@ public class DspApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DspApplication.class, args);
 	}
-
 }
--- src/test/java/com/akul/dsp/DspApplicationTests.java
+++ src/test/java/com/akul/dsp/DspApplicationTests.java
@@ -9,5 +9,4 @@ class DspApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
