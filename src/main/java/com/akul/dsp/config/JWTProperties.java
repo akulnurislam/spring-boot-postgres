a981d910f2faab36152e483d78ec5bf3d129aa4b
akul <nurislamimanudin@gmail.com>
2023-05-11 07:34:33 +0700

    fixing properties class, must be there is a setter

--- src/main/java/com/akul/dsp/config/JWTProperties.java
+++ src/main/java/com/akul/dsp/config/JWTProperties.java
@@ -1,12 +1,12 @@
 package com.akul.dsp.config;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "jwt")
-@Getter
+@Data
 public class JWTProperties {
     private String secret;
     private int expiration;
