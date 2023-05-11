80eb3f46b0709553e881bb7a3b7df80fb6341ef0
akul <nurislamimanudin@gmail.com>
2023-05-11 11:05:28 +0700

    update error-dto include NON_NULL

--- src/main/java/com/akul/dsp/dto/ErrorDTO.java
+++ src/main/java/com/akul/dsp/dto/ErrorDTO.java
@@ -1,5 +1,6 @@
 package com.akul.dsp.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDTO {
     private String timestamp;
     private int status;
