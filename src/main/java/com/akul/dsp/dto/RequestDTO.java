33e129dc92e5a18749c346a3e2aba6e6fdb79627
akul <nurislamimanudin@gmail.com>
2023-05-11 07:05:45 +0700

    request dto

---
+++ src/main/java/com/akul/dsp/dto/RequestDTO.java
@@ -0,0 +1,24 @@
+package com.akul.dsp.dto;
+
+import com.akul.dsp.validator.CapitalAndNumber;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class RequestDTO {
+    @NotBlank
+    @Size(min = 10, max = 13)
+    @Pattern(regexp = "^08\\d+", message = "only number and must be started with 08")
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 60, message = "size must be less than equal 60")
+    private String name;
+
+    @NotBlank
+    @Size(min = 6, max = 16)
+    @CapitalAndNumber(message = "must be containing at least 1 capital letter and 1 number")
+    private String password;
+}
