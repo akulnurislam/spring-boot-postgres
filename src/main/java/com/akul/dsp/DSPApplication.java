2e2d60f64095e2381938857699ef4753c83fe52c
akul <nurislamimanudin@gmail.com>
2023-05-11 07:27:18 +0700

    rename DspApplication to DSPApplication

--- src/main/java/com/akul/dsp/DSPApplication.java
+++ src/main/java/com/akul/dsp/DSPApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DspApplication {
+public class DSPApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DspApplication.class, args);
+		SpringApplication.run(DSPApplication.class, args);
 	}
 }
