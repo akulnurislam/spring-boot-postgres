7a0feacf7a692e97f28c686797a15e3fb2754504
akul <nurislamimanudin@gmail.com>
2023-05-12 14:05:23 +0700

    update login-controller update response with login-response-dto

--- src/main/java/com/akul/dsp/controller/LoginController.java
+++ src/main/java/com/akul/dsp/controller/LoginController.java
@@ -1,14 +1,13 @@
 package com.akul.dsp.controller;
 
 import com.akul.dsp.dto.LoginDTO;
+import com.akul.dsp.dto.LoginResponseDTO;
 import com.akul.dsp.util.JWT;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +16,13 @@ public class LoginController {
     private final JWT jwt;
 
     @PostMapping
-    public ResponseEntity<String> login(@RequestBody @Valid LoginDTO loginDTO) {
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
         System.out.println(loginDTO);
-        return ResponseEntity.ok(jwt.generate(loginDTO.getPhoneNumber()));
+        return ResponseEntity.ok(
+                LoginResponseDTO.builder()
+                        .token(jwt.generate(loginDTO.getPhoneNumber()))
+                        .build()
+        );
     }
 }
