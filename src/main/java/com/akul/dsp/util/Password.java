7f37067c5945aefee91b1268426ec8f882018406
akul <nurislamimanudin@gmail.com>
2023-05-12 17:27:04 +0700

    password util

---
+++ src/main/java/com/akul/dsp/util/Password.java
@@ -0,0 +1,25 @@
+package com.akul.dsp.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Password {
+    public static String hash(String password, String salt) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password + salt);
+    }
+
+    public static boolean verify(String password, String salt, String hashedPassword) {
+        String hashed = hash(password, salt);
+        return hashed.equals(hashedPassword);
+    }
+
+    public static String salt() {
+        SecureRandom random = new SecureRandom();
+        byte[] saltBytes = new byte[16];
+        random.nextBytes(saltBytes);
+        return Base64.getEncoder().encodeToString(saltBytes);
+    }
+}
