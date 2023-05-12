1d8b7197f043921989eebde70d01adcd0f8afd5d
akul <nurislamimanudin@gmail.com>
2023-05-12 17:33:55 +0700

    update user service

--- src/main/java/com/akul/dsp/service/UserService.java
+++ src/main/java/com/akul/dsp/service/UserService.java
@@ -1,11 +1,14 @@
 package com.akul.dsp.service;
 
 import com.akul.dsp.model.User;
+import org.springframework.lang.NonNull;
 
 public interface UserService {
+    @NonNull
     User findOneByPhoneNumber(String phoneNumber);
 
     void create(User user);
 
+    @NonNull
     User updateByPhoneNumber(String phoneNumber, User user);
 }
