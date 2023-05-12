b7a0df53cc85f5d7c89798f6a82dc0ce86c7cbfc
akul <nurislamimanudin@gmail.com>
2023-05-12 14:03:33 +0700

    exclude UserDetailsServiceAutoConfiguration class

--- src/main/java/com/akul/dsp/DSPApplication.java
+++ src/main/java/com/akul/dsp/DSPApplication.java
@@ -2,8 +2,11 @@ package com.akul.dsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		UserDetailsServiceAutoConfiguration.class
+})
 public class DSPApplication {
 
 	public static void main(String[] args) {
