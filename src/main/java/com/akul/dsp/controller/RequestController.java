86b103c5a9ddbcf857880aaf2296d948abbd31f7
akul <nurislamimanudin@gmail.com>
2023-05-12 14:05:52 +0700

    update request-controller add info response status

--- src/main/java/com/akul/dsp/controller/RequestController.java
+++ src/main/java/com/akul/dsp/controller/RequestController.java
@@ -2,11 +2,9 @@ package com.akul.dsp.controller;
 
 import com.akul.dsp.dto.RequestDTO;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -15,6 +13,7 @@ import java.net.URI;
 public class RequestController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> request(@RequestBody @Valid RequestDTO requestDTO) {
         System.out.println(requestDTO);
         return ResponseEntity.created(URI.create("/request")).build();
