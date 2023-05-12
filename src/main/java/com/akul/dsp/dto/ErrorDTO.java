ee7341761f0f1edcced0eae08c65e3b13df5531d
akul <nurislamimanudin@gmail.com>
2023-05-12 14:02:33 +0700

    update error dto

--- src/main/java/com/akul/dsp/dto/ErrorDTO.java
+++ src/main/java/com/akul/dsp/dto/ErrorDTO.java
@@ -3,10 +3,12 @@ package com.akul.dsp.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Builder
+@ToString
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDTO {
