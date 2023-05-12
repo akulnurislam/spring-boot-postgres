983082140a0932545d16333af4d4ad173680d4f6
akul <nurislamimanudin@gmail.com>
2023-05-12 14:06:18 +0700

    update name-controller add security schema

--- src/main/java/com/akul/dsp/controller/NameController.java
+++ src/main/java/com/akul/dsp/controller/NameController.java
@@ -1,17 +1,25 @@
 package com.akul.dsp.controller;
 
 import com.akul.dsp.dto.NameDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/name")
+@SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, scheme = "bearer")
 public class NameController {
 
     @GetMapping
+    @Operation(description = "API get name", security = @SecurityRequirement(name = "Authorization"))
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> getName(
             @RequestAttribute("phoneNumber") String phoneNumber
     ) {
@@ -19,6 +27,8 @@ public class NameController {
     }
 
     @PutMapping
+    @Operation(description = "API update name", security = @SecurityRequirement(name = "Authorization"))
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Void> updateName(@RequestBody @Valid NameDTO nameDTO) {
         System.out.println(nameDTO);
         return ResponseEntity.ok().build();
