a748f820ab1984dab2e5cab4d837aa8251bb3f23
akul <nurislamimanudin@gmail.com>
2023-05-14 15:44:47 +0700

    update open-api config add log info swagger index

--- src/main/java/com/akul/dsp/config/OpenAPIConfig.java
+++ src/main/java/com/akul/dsp/config/OpenAPIConfig.java
@@ -4,18 +4,32 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Configuration
 public class OpenAPIConfig {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    @Profile("!test")
+    public ApplicationRunner applicationRunner() {
+        return args -> log.info("Swagger UI: http://localhost:{}/swagger-ui/index.html", port);
+    }
 
     @Bean
     public OpenAPI openAPI() {
